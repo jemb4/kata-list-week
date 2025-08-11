55947b5c3c241dd2a73e8f158f1da10d8d30f3d7
Jesus Martin <jesus8.mb@gmail.com>
2025-08-11 23:21:58 +0200

    feat: creado método getDays

---
+++ src/main/java/dev/jesus/KataWeek.java
@@ -0,0 +1,52 @@
+package dev.jesus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KataWeek {
+
+  private List<String> weekDaysList;
+
+  public KataWeek() {
+    createListDays();
+  }
+
+  public void createListDays() {
+    weekDaysList = new ArrayList<>();
+    weekDaysList.add("Lunes");
+    weekDaysList.add("Martes");
+    weekDaysList.add("Miércoles");
+    weekDaysList.add("Jueves");
+    weekDaysList.add("Viernes");
+    weekDaysList.add("Sábado");
+    weekDaysList.add("Domingo");
+  }
+
+  public List<String> getDays() {
+    return weekDaysList;
+  }
+
+  public int getWeekSize() {
+    return weekDaysList.size();
+  }
+
+  public void deleteDay(String day) {
+
+  }
+
+  public String getDay(int numbDay) {
+    return "";
+  }
+
+  public boolean dayExist(String day) {
+    return true;
+  }
+
+  public void orderList() {
+
+  }
+
+  public void clearList() {
+
+  }
+}
---
+++ src/test/java/dev/jesus/KataWeekTest.java
@@ -0,0 +1,60 @@
+package dev.jesus;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class KataWeekTest {
+
+  private KataWeek kataWeek;
+
+  @BeforeEach
+  void setUp() {
+    kataWeek = new KataWeek();
+  }
+
+  @Test
+  void testClearList() {
+
+  }
+
+  @Test
+  void testCreateListDays() {
+
+  }
+
+  @Test
+  void testDayExist() {
+
+  }
+
+  @Test
+  void testDeleteDay() {
+
+  }
+
+  @Test
+  void testGetDay() {
+
+  }
+
+  @Test
+  void testGetDays() {
+
+  }
+
+  @Test
+  void testGetWeekSize() {
+
+  }
+
+  @Test
+  void testOrderList() {
+
+  }
+}
